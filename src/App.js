02ffd1b8bcf238421c83e7d1bc549a1abf5b9fec
zadigg <62830716+zadigg@users.noreply.github.com>
2022-06-28 19:06:38 +0300

    check if there is deleted file

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import GitStar from "./components/GitStar";
+import Header from "./components/Header";
+import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <GitStar />
+      <Header />
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/components/GitStar.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const GitStar = () => {
+  return (
+    <a
+      href="https://github.com/zadigg/Tailwind-Background-Designes"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <div className="w-full bg-black text-white py-3 text-lg ">
+        <div className="flex justify-center">
+          <span className="animate-pulse">🌟 </span> Star
+          Tailwind-Background-Designes on GitHub{" "}
+          <span className="animate-pulse">🌟 </span>
+        </div>
+      </div>
+    </a>
+  );
+};
+
+export default GitStar;
