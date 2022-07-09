b4556b83c648031c806a7fe0e801402625d6b429
zadigg <62830716+zadigg@users.noreply.github.com>
2022-07-09 21:21:29 +0300

    check if there is deleted file

--- src/pages/Designs/Design1.js
+++ src/pages/Designs/Design1.js
@@ -20,6 +20,12 @@ const Design1 = () => {
         <div className={` ${myFunc(h)} w-20  bg-white `}></div>
 
         {myFunc(h)}
+        {myFunc(h)}
+
+        {myFunc(h)}
+
+        {myFunc(h)}
+
 
         <div className={` w-20 h-20 bg-blue-500`}></div>
         <div className={` w-80 h-20 bg-red-500 rotate-45 animate-pulse`}></div>
